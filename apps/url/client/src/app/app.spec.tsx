e4fb3bab81a9e74c1cec20431b91f81199189780
Alison Ryan <Ryan.alis@northeastern.edu>
2023-07-06 16:42:54 -0400

    URL verification

--- apps/url/client/src/app/app.spec.tsx
+++ apps/url/client/src/app/app.spec.tsx
@@ -1,11 +1,11 @@
 import { render, screen } from '@testing-library/react';
 
-import UrlList from './urlList';
+import ListOfUrls from './urlList';
 import { Shortened } from './types';
 
 describe('UrlList', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<UrlList urls={[]} />);
+    const { baseElement } = render(<ListOfUrls urls={[]} />);
     expect(baseElement).toBeTruthy();
   });
 
@@ -14,7 +14,7 @@ describe('UrlList', () => {
       { original: 'https://c4cneu.com', short: 'http://short.com/s/0' },
     ];
 
-    render(<UrlList urls={urls} />);
+    render(<ListOfUrls urls={urls} />);
     expect(screen.getByText(urls[0].original, { exact: false })).toBeTruthy();
     expect(screen.getByText(urls[0].short, { exact: false })).toBeTruthy();
   });
--- apps/url/client/src/app/urlList.tsx
+++ apps/url/client/src/app/urlList.tsx
@@ -5,19 +5,50 @@ type UrlListProps = {
   urls: Array<Shortened>;
 };
 
-export const UrlList: React.FC<UrlListProps> = ({ urls }) => {
+type ShortUrl = {
+  url: Shortened;
+};
+
+export const ListOfUrls: React.FC<UrlListProps> = ({ urls }) => {
   return (
     <UnorderedList id="urlList" textAlign="left">
       {urls.map((u) => (
-        <ListItem>
-          <Link href={u.short} color="teal.500">
-            {u.short}
-          </Link>{' '}
-          - {u.original}
-        </ListItem>
+        <ListUrl url={u} />
       ))}
     </UnorderedList>
   );
 };
 
-export default UrlList;
+const ListUrl: React.FC<ShortUrl> = ({ url }) => {
+  const urlPattern =
+    /(?:https?):\/\/(\w+:?\w*)?(\S+)(:\d+)?(\/|\/([\w#!:.?+=&%!\-\/]))?/;
+  if (!!urlPattern.test(url.original)) {
+    return <UrlList url={url} />;
+  } else {
+    return <StringList url={url} />;
+  }
+};
+
+const StringList: React.FC<ShortUrl> = ({ url }) => {
+  return (
+    <ListItem>
+      {url.original} is an invalid link. Please try again with the proper link
+      format.
+    </ListItem>
+  );
+};
+
+const UrlList: React.FC<ShortUrl> = ({ url }) => {
+  return (
+    <UnorderedList id="urlList" textAlign="left">
+      <ListItem>
+        <Link href={url.short} color="teal.500">
+          {url.short}
+        </Link>{' '}
+        - {url.original}
+      </ListItem>
+    </UnorderedList>
+  );
+};
+
+export default ListOfUrls;
